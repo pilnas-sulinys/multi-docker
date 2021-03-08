138baaa949b851834df32e3c7920eda8c8aa78d4
pilnas sulinys <pilnas.sulinys@gmail.com>
2021-03-07 23:11:26 -0800

    Update page to new version

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator
+            New version of Fib calculator 
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
